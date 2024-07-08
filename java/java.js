c6b4ae15760c9e5a8478bad3f411e914c1f1b1ac
thiyagus06 <steditzofficial@gmail.com>
2024-07-08 12:27:17 +0530

    js practices

---
+++ java/java.js
@@ -0,0 +1,12 @@
+function func1(){
+    var func1=document.getElementById ("num1").value;
+    var func2=document.getElementById ("num2").value;
+    console.log(num1*num2);
+
+    document.write(num1*num2);
+    document.write("br")
+    document.write(num1/num2);
+    document.write("br")
+    document.write(num1%num2);
+
+}
